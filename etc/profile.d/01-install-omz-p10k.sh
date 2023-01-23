236c5a5a9d51cb3e765840932401fbd54bb65625
kramfs <rudelsaldivar@gmail.com>
2023-01-23 17:09:21 +0800

    Added logic to detect and install omz and p10k

--- etc/profile.d/01-install-omz-p10k.sh
+++ etc/profile.d/01-install-omz-p10k.sh
@@ -7,6 +7,7 @@ if test "$(id -u)" -gt "0" && test -d "$HOME"; then
     if test ! -e "$HOME"/.oh-my-zsh; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-        echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+        #echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+        sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
     fi
 fi
