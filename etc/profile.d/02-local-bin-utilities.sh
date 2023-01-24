fc45f827487e05313df98c72c8eb2e009819758e
kramfs <rudelsaldivar@gmail.com>
2023-01-24 11:38:49 +0800

    Add host-spawn to user path

--- etc/profile.d/02-local-bin-utilities.sh
+++ etc/profile.d/02-local-bin-utilities.sh
@@ -2,7 +2,7 @@
 if test "$(id -u)" -gt "0" && test -d "$HOME"; then
     if test ! -e "$HOME"/.local/bin; then
         mkdir -p $HOME/.local/bin
+        curl -Lo $HOME/.local/bin/host-spawn https://github.com/1player/host-spawn/releases/download/1.4.0/host-spawn-x86_64
+        chmod +x $HOME/.local/bin/host-spawn
     fi
-    curl -Lo $HOME/.local/bin/host-spawn https://github.com/1player/host-spawn/releases/download/1.4.0/host-spawn-x86_64
-    chmod +x $HOME/.local/bin/host-spawn
 fi
