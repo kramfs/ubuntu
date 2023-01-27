3994a6e265096eee6db37dee71d016e85d340a98
kramfs <rudelsaldivar@gmail.com>
2023-01-27 12:05:35 +0800

    Add add alternative location for the host-spawn utility

--- etc/profile.d/02-local-bin-utilities.sh
+++ etc/profile.d/02-local-bin-utilities.sh
@@ -4,5 +4,7 @@ if test "$(id -u)" -gt "0" && test -d "$HOME"; then
         mkdir -p $HOME/.local/bin
         curl -Lo $HOME/.local/bin/host-spawn https://github.com/1player/host-spawn/releases/download/1.4.0/host-spawn-x86_64
         chmod +x $HOME/.local/bin/host-spawn
+        #curl -Lo /usr/bin/host-spawn https://github.com/1player/host-spawn/releases/download/1.4.0/host-spawn-x86_64
+        #chmod +x /usr/bin/host-spawn
     fi
 fi
